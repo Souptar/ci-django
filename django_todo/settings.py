aabade0be2a7098bc75366a17cf8ea9f56dcabe9
Souptar <jamjoy0@gmail.com>
2021-02-24 16:03:02 +0000

    import os

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'm7ju@)r)vis$4^hnduu2&_fcu(h(fnb_6&dy#vk7b45)s6)d#n')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'm7ju@)r)vis$4^hnduu2&_fcu(h(fnb_6&dy#vk7b45)s6)d#n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
